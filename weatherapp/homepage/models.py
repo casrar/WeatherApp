9c40c239ee995e0a319353db1e45fbca74061516
Casrar <casey.j.morar@gmail.com>
2022-08-10 13:46:23 -0400

    edited admin page, created db, etc

--- weatherapp/homepage/models.py
+++ weatherapp/homepage/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 # Create your models here.
-#Model of a location (city, state[nullable], country)
+class Location(models.Model):
+    city_name = models.CharField(max_length=200)
+    state_name = models.CharField(max_length=200, blank=True, null=True)
+    country_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.city_name + ', '  + self.state_name + ', ' + self.country_name
--- weatherapp/homepage/views.py
+++ weatherapp/homepage/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from dotenv import dotenv_values
+import requests
 
+LIMIT = '5'
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the homepage index.")
+    config = dotenv_values(".env")
+    city = 'pittsburgh'
+    state = 'PA'
+    country = 'USA'
+    response = requests.get('http://api.openweathermap.org/geo/1.0/direct?q='+ city 
+        +','+ state 
+        +','+ country 
+        +'&limit='+ LIMIT 
+        +'&appid='+ config['OPENWEATHERAPI'])
+
+    print(response.content) 
+
+    return HttpResponse(response.content)
