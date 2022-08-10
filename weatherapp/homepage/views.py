464c2fee45191f7cd3bf6e9f248447fa2408b327
Casrar <casey.j.morar@gmail.com>
2022-08-10 14:17:04 -0400

    created functions to locate and define weather

--- weatherapp/homepage/views.py
+++ weatherapp/homepage/views.py
@@ -4,19 +4,32 @@ from dotenv import dotenv_values
 import requests
 
 LIMIT = '5'
+CONFIG = dotenv_values(".env")
 
 # Create your views here.
 def index(request):
-    config = dotenv_values(".env")
-    city = 'pittsburgh'
-    state = 'PA'
-    country = 'USA'
+    lat, lon = geolocate('pittsburgh', 'PA', 'USA') #testing values
+    return HttpResponse(weather(lat, lon))
+   
+def geolocate(city, state, country):
     response = requests.get('http://api.openweathermap.org/geo/1.0/direct?q='+ city 
         +','+ state 
         +','+ country 
         +'&limit='+ LIMIT 
-        +'&appid='+ config['OPENWEATHERAPI'])
+        +'&appid='+ CONFIG['OPENWEATHERAPI'])
 
-    print(response.content) 
+    if (response.status_code != 200):
+        return HttpResponse('Error')
+    
+    response_json = response.json()
+    return (response_json[0]['lat'], response_json[0]['lon'])
 
-    return HttpResponse(response.content)
+def weather(lat, lon):
+    response = requests.get('https://api.openweathermap.org/data/2.5/weather?lat='+ str(lat) 
+        +'&lon=' + str(lon) 
+        +'&appid='+ CONFIG['OPENWEATHERAPI'])
+
+    if (response.status_code != 200):
+        return HttpResponse('Error')
+
+    return response.text
