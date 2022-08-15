84d33b4b80d7aad8ef3ca19f4b13fc6ce4a2cd19
Casrar <casey.j.morar@gmail.com>
2022-08-14 21:53:00 -0400

    removing \weather route

--- weatherapp/weatherapp/urls.py
+++ weatherapp/weatherapp/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('weather.urls')),
-    path('weather/', include('weather.urls')),
+    #path('weather/', include('weather.urls')),
     path('admin/', admin.site.urls),
 ]
